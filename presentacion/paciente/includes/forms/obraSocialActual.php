94f136a75f7abc1a45c7bc8ba6afaadfc17e19dc
Juan Ferreyra <juangabrielferreyra93@gmail.com>
2016-08-22 08:17:09 -0300

    valido campos null en fechas de obra social

--- presentacion/paciente/includes/forms/obraSocialActual.php
+++ presentacion/paciente/includes/forms/obraSocialActual.php
@@ -17,18 +17,20 @@ $(document).ready(function() {
               var idosoc = $("#idFichaOsoc").val();
               $("#dialog_subcontent").load("includes/forms/modificarObraSocial.php",{id:idosoc, tipodoc:<?php echo $tipodoc; ?>, nrodoc:<?php echo $nrodoc; ?>});
        });
-})
+});
 </script>
 <input type="hidden" id="idFichaOsoc" value="<?php echo $obras_social['id']; ?>">
 <label>Obra Social :</label><?php echo $obras_social['obra_social']; ?><br>
 <label>Nro de Afiliado :</label><?php echo $obras_social['nro_afiliado']; ?><br>
 <label>Empresa :</label><?php echo $obras_social['empresa_nombre']; ?><br>
 <label>Direccion :</label><?php echo $obras_social['empresa_direccion']; ?><br>
-<label>Fecha de emision :</label><?php echo Utils::sqlDateToHtmlDate($obras_social['fecha_emision']); ?><br>
-<label>Fecha de vencimiento :</label><?php echo Utils::sqlDateToHtmlDate($obras_social['fecha_vencimiento']); ?><br>
-<label>Ultima Modificacion</label><br>
+<label>Fecha de emision :</label><?php if($obras_social['fecha_vencimiento']!=null){ echo Utils::sqlDateToHtmlDate($obras_social['fecha_emision']); }?><br>
+<label>Fecha de vencimiento :</label><?php if($obras_social['fecha_vencimiento']!=null){ echo Utils::sqlDateToHtmlDate($obras_social['fecha_vencimiento']); }?><br>
 
-<?php echo Utils::sqlDateToHtmlDate($obras_social['fecha_creacion']); ?><br>
+<?php if($obras_social['fecha_creacion']!=null){
+  echo "<label>Ultima Modificacion</label><br>".Utils::sqlDateToHtmlDate($obras_social['fecha_creacion'])."<br>";
+}
+?>
 
 <button class="button-secondary" id="modificarOsoc" data-dialog="somedialog" ><?php
        if($obras_social==null){
@@ -38,17 +40,17 @@ $(document).ready(function() {
        }?></button>
 
 
-       <script>
-      (function() {
+<script>
+  (function() {
 
-        //dialogo de laboratorio
-        var dlgtrigger = document.querySelector( '[data-dialog]' ),
+    //dialogo de laboratorio
+    var dlgtrigger = document.querySelector( '[data-dialog]' ),
 
-          somedialog = document.getElementById( dlgtrigger.getAttribute( 'data-dialog' ) ),
+      somedialog = document.getElementById( dlgtrigger.getAttribute( 'data-dialog' ) ),
 
-          dlg = new DialogFx( somedialog );
+      dlg = new DialogFx( somedialog );
 
-        dlgtrigger.addEventListener( 'click', dlg.toggle.bind(dlg) );
+    dlgtrigger.addEventListener( 'click', dlg.toggle.bind(dlg) );
 
-        })();
-    </script>
+    })();
+</script>
