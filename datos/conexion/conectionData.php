92107f07f2e5c7b8960bf5c6d92bddb26c9f7788
Juan Ferreyra <juangabrielferreyra93@gmail.com>
2016-11-14 16:13:28 -0300

    agrego un conetionData de ejemplo y borro el original para el commit

--- datos/conexion/conectionData.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("HOSTLocal", 'localhost');
-define("DB", 'turnos');
-define("USRDBAdmin", 'root');
-define("PASSDBAdmin", 'f0rm4t1c4');
-
-?>
---
+++ datos/conexion/example_conectionData.php
@@ -0,0 +1,10 @@
+<?php
+
+define("HOSTLocal", 'localhost');
+define("DB", 'turnos');
+define("USRDBAdmin", 'turnero');
+define("PASSDBAdmin", 'turn3r0');
+
+define("Business",'1');//Defino el id de la empresa
+
+?>
